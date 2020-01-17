6161ff4060388d1732c896b560922345f401c93f
Web Development <info@toniramon.es>
2020-01-17 17:52:42 +0100

    added first td with vue

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-      title: 'Shopping List'
+        title: 'Shopping List',
+        items: [
+            {
+                id: Math.ceil(Math.random() * 10000),
+                name: 'test',
+                quantity: 2,
+                price: 3
+            },
+            {
+                id: Math.ceil(Math.random() * 10000),
+                name: 'test2222',
+                quantity: 10,
+                price: 5
+            }
+        ]
     }
-  })
+})
