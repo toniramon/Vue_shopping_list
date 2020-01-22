e2a68268c439e2443b89b24fbf7b51c54c73b155
Web Development <info@toniramon.es>
2020-01-22 20:58:36 +0100

    bind data and update input

--- script.js
+++ script.js
@@ -2,19 +2,42 @@ var app = new Vue({
     el: '#app',
     data: {
         title: 'Shopping List',
-        items: [
-            {
-                id: Math.ceil(Math.random() * 10000),
-                name: 'test',
-                quantity: 2,
-                price: 3
-            },
-            {
-                id: Math.ceil(Math.random() * 10000),
-                name: 'test2222',
-                quantity: 10,
-                price: 5
+        name: '',
+        price: '',
+        items: []
+    },
+    computed: {
+        totalAmount () {
+            let sumaTotal = 0;
+            this.items.forEach(item => {
+                sumaTotal += item.quantity * item.price;
+            });
+            return sumaTotal;
+        }
+    },
+    methods: {
+        addItem: function() {
+            if (this.name && this.price) {
+                let item = {
+                    name: this.name,
+                    quantity: 1,
+                    price: this.price
+                };
+                this.items.push(item);
             }
-        ]
+            
+            
+            this.updateTotalAmount();
+        },
+        deleteItem: function (index){
+            this.items.splice(index, 1); //delete 1 element from the array at the position index
+        },
+        updateItem(index, quantity) {
+            this.items[index].quantity = quantity;
+            this.updateTotalAmount;
+        },
+        updateTotalAmount() {
+            this.totalAmount;
+        }
     }
 })
